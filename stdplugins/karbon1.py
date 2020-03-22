54bc03eac4ec676f5250722e0b5c4678b7b5e390
jayzee427 <61549029+jayzee427@users.noreply.github.com>
2020-03-22 17:54:24 +0530

    Update karbon1.py

--- stdplugins/karbon1.py
+++ stdplugins/karbon1.py
@@ -62,7 +62,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Karbon1 by [@NeoMatrix90](https://www.github.com/prono69/PepeBot)",
+         caption="Karbon1 by [@jH4cker]", 
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
