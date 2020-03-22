50695b67564804966cbe18becae21539d23b41a2
jayzee427 <61549029+jayzee427@users.noreply.github.com>
2020-03-22 11:14:40 +0530

    Lol
    Don't like promoting

--- stdplugins/karbon2.py
+++ stdplugins/karbon2.py
@@ -62,7 +62,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Karbon2 by [@NeoMatrix90](https://www.github.com/prono69/PepeBot)",
+         caption="Karbon2 
          force_document=True,
          reply_to=e.message.reply_to_msg_id,
          )
